e03099ea94bad82ceb2751085bf2ba222edb4294
Olek Osikowicz <tupolewcx@gmail.com>
2024-09-16 16:33:42 +0200

    Add a scrit that verifies random search in oracle quality

---
+++ olek/run_verification.py
@@ -0,0 +1,13 @@
+from scenario_runner import ScenarioRunner
+from tqdm import tqdm
+from pathlib import Path
+
+if __name__ == "__main__":
+
+    DATA_DIR = Path("olek/data")
+    RS_DIR = DATA_DIR / "random_search"
+    VERIFICATION_SAVE_DIR = DATA_DIR / "verification"
+
+    seeds = [path.stem for path in RS_DIR.glob("**/*.json")]
+    for seed in tqdm(seeds):
+        ScenarioRunner(VERIFICATION_SAVE_DIR, seed).run_scenario(record_gif=True)
