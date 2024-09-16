a6750b5e57dfef396b4b28ef8eac3bb19470f593
Olek Osikowicz <tupolewcx@gmail.com>
2024-09-16 15:38:19 +0200

    Fixing bugs in random search and improving log output

--- olek/run_random_search.py
+++ olek/run_random_search.py
@@ -1,6 +1,6 @@
 from scenario_runner import ScenarioRunner, logger
 
-import itertools
+from itertools import count, product
 import random
 import time
 
@@ -10,14 +10,18 @@ SAVE_DIR = "olek/data/random_search"
 def do_random_search(rep, dr, dt, time_budget=60):
     logger.info(f"Starting random search for: {dr = :3}, {dt = } {rep = }")
     start_ts = time.time()
-
-    while time.time() - start_ts < time_budget:
+    for it in count():
+        logger.info(f"Starting iteration {it =}")
         env_seed = random.randint(100, 10**10)
-        ScenarioRunner(f"{SAVE_DIR}/{rep}", env_seed, dr, dt).run_scenario(
+        ScenarioRunner(f"{SAVE_DIR}/{rep}/{it}", env_seed, dr, dt).run_scenario(
             record_gif=True, repeat=True
         )
 
-    logger.info(f"Random search for: {dr = :3}, {dt = } {rep = } finished!")
+        if time.time() - start_ts > time_budget:
+            logger.info(f"Time elapsed!")
+            break
+
+    logger.info(f"Random search finished for: {dr = :3}, {dt = } {rep = }!")
 
 
 if __name__ == "__main__":
@@ -25,12 +29,11 @@ if __name__ == "__main__":
 
     TIME_BUDGET = 10 * 60  # 10 mins
 
-    dr_range = [5, 10, 15]
-    # dr_range = [5, 10, 15, 20]
-    dt_range = [0.02, 0.03]
-    # dt_range = [0.02, 0.03, 0.04]
-    for rep in range(10):
-        for dr, dt in itertools.product(dr_range, dt_range):
-            do_random_search(rep, dr, dt, TIME_BUDGET)
+    N_REPETITIONS = 10
+    dr_range = [5, 10, 15, 20]
+    dt_range = [0.02, 0.03, 0.04]
+
+    for rep, dr, dt in product(range(N_REPETITIONS), dr_range, dt_range):
+        do_random_search(rep, dr, dt, TIME_BUDGET)
 
     logger.info("All experiments finished :))")
