a2abb7fa97c016be8e0f2e4dd80cd9c357621680
Olek Osikowicz <tupolewcx@gmail.com>
2024-10-28 13:35:15 +0000

    Run flaky check, in deterministic manner

---
+++ olek/run_flaky_check.py
@@ -0,0 +1,13 @@
+from scenario_runner import ScenarioRunner
+from tqdm import tqdm
+import itertools
+
+if __name__ == "__main__":
+
+    REPETITIONS_RANGE = range(10)
+    SEED_RANGE = range(200)
+    # High fidelity only
+
+    for rep, seed in tqdm(itertools.product(REPETITIONS_RANGE, SEED_RANGE)):
+        SAVE_DIR = f"data/flaky_check/{rep}"
+        ScenarioRunner(SAVE_DIR, seed).run_scenario()
