a8e339707e35e5a00130adb5c0d672d90f89826f
Olek Osikowicz <tupolewcx@gmail.com>
2024-12-04 11:47:41 +0100

    Go back to recording gifs

--- olek/run_static_benchmarking.py
+++ olek/run_static_benchmarking.py
@@ -4,10 +4,12 @@ import itertools
 
 if __name__ == "__main__":
 
-    SEED_RANGE = range(0, 1000)
+    SEED_RANGE = range(0, 100)
     DR_RANGE = [5, 10, 15, 20]
     DT_RANGE = [0.02, 0.03, 0.04]
 
-    SAVE_DIR = "data/benchmarking/nocars"
+    SAVE_DIR = "data/v2/benchmarking"
     for seed, dr, dt in tqdm(itertools.product(SEED_RANGE, DR_RANGE, DT_RANGE)):
-        ScenarioRunner(SAVE_DIR, seed, dr, dt, traffic_density=0.0).run_scenario()
+        ScenarioRunner(SAVE_DIR, seed, dr, dt, traffic_density=0.0).run_scenario(
+            record_gif=True,
+        )
