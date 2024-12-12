5254c3f3ab36d9a4bda5ca51de6230ec2aa06e3e
Olek Osikowicz <tupolewcx@gmail.com>
2024-12-12 15:41:25 +0000

    Push past work to preliminary studies

--- olek/run_static_benchmarking.py
+++ olek/run_static_benchmarking.py
@@ -4,11 +4,11 @@ import itertools
 
 if __name__ == "__main__":
 
-    SEED_RANGE = range(0, 100)
+    SEED_RANGE = range(0, 200)
     DR_RANGE = [5, 10, 15, 20]
     DT_RANGE = [0.02, 0.03, 0.04]
 
-    SAVE_DIR = "data/v2/benchmarking"
+    SAVE_DIR = "data/benchmarking"
     for seed, dr, dt in tqdm(itertools.product(SEED_RANGE, DR_RANGE, DT_RANGE)):
         ScenarioRunner(SAVE_DIR, seed, dr, dt, traffic_density=0.0).run_scenario(
             record_gif=True,
