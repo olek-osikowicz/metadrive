fdfee7fd03b60c29a52cab2eebcc4d31da5eb4c9
Olek Osikowicz <tupolewcx@gmail.com>
2025-02-10 16:14:53 +0000

    Record videos not GIFs, calculate FPS from dr and dt

--- olek/run_static_benchmarking.py
+++ olek/run_static_benchmarking.py
@@ -1,17 +1,30 @@
+from pathlib import Path
 from utils.scenario_runner import ScenarioRunner
 from tqdm import tqdm
 import itertools
 
+
+def get_dt(fps):
+    # Assume default value
+    DECISION_REPEAT = 5
+    interval = 1 / fps
+    dt = interval / DECISION_REPEAT
+    return dt
+
+
 if __name__ == "__main__":
 
-    SEED_RANGE = range(0, 200)
-    DR_RANGE = [5, 10, 15, 20]
-    DT_RANGE = [0.02, 0.03, 0.04]
+    SAVE_DIR = Path("/media/olek/2TB_HDD/metadrive-data/playground/high_fid_test")
+    SEED_RANGE = range(0, 1000)
+    REPS = 10
+    FPS_RANGE = [10, 20, 30, 40, 50, 60]
 
-    SAVE_DIR = "data/benchmarking"
-    for seed, dr, dt in tqdm(itertools.product(SEED_RANGE, DR_RANGE, DT_RANGE)):
-        ScenarioRunner(SAVE_DIR, seed, dr, dt, traffic_density=0.0).run_scenario(
-            record_gif=True,
-            repeat=True,
-            save_map=True,
-        )
+    for seed in SEED_RANGE:
+        for rep in range(REPS):
+            save_path = SAVE_DIR / f"{rep}"
+            for fps in FPS_RANGE:
+                dt = get_dt(fps)
+                ScenarioRunner(save_path, seed, dt=dt, traffic_density=0.0).run_scenario(
+                    record=True,
+                    repeat=True,
+                )
