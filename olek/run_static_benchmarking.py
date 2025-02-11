b2160f4cfcc0cfa2a253fbbe6e8a4a67532a3bb1
Olek Osikowicz <tupolewcx@gmail.com>
2025-02-11 16:09:02 +0000

    Use multiprocessing to execute many scenarios

--- olek/run_static_benchmarking.py
+++ olek/run_static_benchmarking.py
@@ -2,6 +2,13 @@ from pathlib import Path
 from utils.scenario_runner import ScenarioRunner
 from tqdm import tqdm
 import itertools
+from tqdm.contrib.concurrent import process_map
+from tqdm.contrib.logging import logging_redirect_tqdm
+import multiprocessing
+
+multiprocessing.set_start_method("spawn", force=True)
+
+HDD_DIR = Path("/media/olek/2TB_HDD/metadrive-data/playground")
 
 
 def get_dt(fps):
@@ -12,18 +19,26 @@ def get_dt(fps):
     return dt
 
 
+def run_scenario(args):
+
+    seed, use_cars, rep, fps = args
+    traffic_density = 0.1 if use_cars else 0.0
+    save_dir = HDD_DIR / "withcars" if use_cars else HDD_DIR / "nocars"
+
+    save_dir = save_dir / f"{rep}"
+    dt = get_dt(fps)
+    ScenarioRunner(save_dir, seed, dt=dt, traffic_density=traffic_density).run_scenario(
+        record=True
+    )
+
+
 if __name__ == "__main__":
 
-    SAVE_DIR = Path("/media/olek/2TB_HDD/metadrive-data/playground")
-    SEED_RANGE = range(100)
+    SEED_RANGE = range(1000)
     REPS = 10
-    FPS_RANGE = [10, 20, 30, 40, 50, 60]
-    jobs = list(itertools.product(SEED_RANGE, range(REPS), FPS_RANGE))
-
-    for seed, rep, fps in jobs:
-
-        save_path = SAVE_DIR / f"{rep}"
-        dt = get_dt(fps)
-        ScenarioRunner(save_path, seed, dt=dt, traffic_density=0.0).run_scenario(
-            record=True, repeat=True, save_map=True
-        )
+    FPS_RANGE = [60, 50, 40, 30, 20, 10]
+    use_cars = [True, False]
+    jobs = list(itertools.product(SEED_RANGE, use_cars, range(REPS), FPS_RANGE))
+    print(jobs)
+    with logging_redirect_tqdm():
+        process_map(run_scenario, jobs, max_workers=16)
