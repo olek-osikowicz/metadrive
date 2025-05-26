380fb4068899c850d1db06deb913b5abfece4e80
Olek Osikowicz <tupolewcx@gmail.com>
2025-05-26 17:23:47 +0100

    Parametrize the search with initialization ratio

--- olek/run_search.py
+++ olek/run_search.py
@@ -12,28 +12,40 @@ N_PROCESSES = 10
 
 
 def do_search_wrapper(args):
+
+    bayesopt_initialization_ratio = 1 - args[0]
+    logger.info(
+        f"Running search with initialization ratio: {bayesopt_initialization_ratio} {args[1:]}"
+    )
+    path = f"/media/olek/8TB_HDD/metadrive-data/initialization/{int(args[0] * 100)}-initialization"
+    logger.info(f"Search root dir: {path}")
     return do_search(
-        *args,
-        bayesopt_initialization_ratio=0.95,  # use x% of the budget for BayesOpt
-        search_root_dir="/media/olek/8TB_HDD/metadrive-data/initialization/small-initialization",
+        *args[1:],
+        bayesopt_initialization_ratio=bayesopt_initialization_ratio,
+        search_root_dir=path,
     )
 
 
 if __name__ == "__main__":
     multiprocessing.set_start_method("spawn", force=True)
 
-    logger.info(f"Epsilon Fidelity Experiments!")
+    logger.info(f"Initialization budget Experiments!")
 
-    # fids = FIDELITY_RANGE.copy()
-    fids = [60, "multifidelity_0.00"]
+    initialization_budget = [0.05, 0.15, 0.20]
 
-    search_jobs = list(product(range(N_REPETITIONS), ["bayesopt_ucb"], fids))
-
-    # search_jobs.extend(product(range(N_REPETITIONS), ["randomsearch"], fids))
+    search_jobs = list(
+        product(
+            initialization_budget,
+            range(N_REPETITIONS),
+            ["bayesopt_ucb"],
+            ["multifidelity_0.10"],
+        )
+    )
 
     logger.info(f"Search jobs: {search_jobs} {len(search_jobs) = }")
     with multiprocessing.Pool(N_PROCESSES, maxtasksperchild=1) as p:
 
         for _ in p.imap_unordered(do_search_wrapper, search_jobs, chunksize=1):
             pass
+
     logger.info("All experiments finished :))")
